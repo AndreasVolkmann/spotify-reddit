41eb2f9e12aa545035b77917dc71da88bfa5baf8
AndreasVolkmann <avo@apurebase.com>
2018-06-10 11:53:12 +0200

    remove outdated arg, inc version, add deploy task to travis

--- src/test/kotlin/me/avo/spottit/config/ArgumentsTest.kt
+++ src/test/kotlin/me/avo/spottit/config/ArgumentsTest.kt
@@ -2,18 +2,16 @@ package me.avo.spottit.config
 
 import org.amshove.kluent.shouldBe
 import org.amshove.kluent.shouldBeEqualTo
-import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
 internal class ArgumentsTest {
 
     @Test fun `parse args correctly`() {
         val configPath = "config.yml"
-        val args = Arguments(arrayOf("-c", configPath, "-ma", "-aa", "-r", "-h"))
+        val args = Arguments(arrayOf("-c", configPath, "-ma", "-r", "-h"))
         with(args) {
             this.configPath shouldBeEqualTo configPath
             manualAuth shouldBe true
-            automaticAuth shouldBe true
             doRefresh shouldBe true
             help shouldBe true
         }
